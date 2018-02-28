63e95d6f284cea42358ca874716e640b004b26e5
kishan <kishan@qburst.com>
2018-02-28 16:35:32 +0530

    db polling

--- db_poller.py
+++ db_poller.py
@@ -1,5 +1,1 @@
-import json
-
-x = [ { 'job_id': "1", 'status': "NOT_STARTED"}, { 'job_id': "2", 'status': "PR_APPROVED"}, { 'job_id': "3", 'status': "NOT_STARTED"}, { 'job_id': "1", 'status': "PR_APPROVED"} ]
-
-print(json.dumps(x))
+print("""{ "pending": [4, 8, 15, 16, 23, 42], "approved": [4, 8, 15, 16, 23, 42] }""")
